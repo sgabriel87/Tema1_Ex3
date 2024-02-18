32e73d15d5e145697e00d6955dcb71e2f76e0aba
Gabriel <Gabriel@GABRIEL>
2024-02-18 19:56:53 +0200

    Add project files.

---
+++ Tema1_Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Ex3
+Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+tastaura
+*/
+
+Console.WriteLine("Introduceți un număr întreg:");
+int numar = Convert.ToInt32(Console.ReadLine());
+
+int ultimaCifra = numar % 10;
+
+// Afisarea rezultatului
+Console.WriteLine($"Ultima cifră a numărului este: {ultimaCifra}");
